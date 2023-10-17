fed9f308f9c40d1aedb07d473dc6088eb288a514
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2023-10-17 21:58:29 +0200

    5.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 }
 
 group = "com.github.angeschossen"
-version = "5.12.1"
+version = "5.13.0"
 description = "ChestProtectAPI"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
