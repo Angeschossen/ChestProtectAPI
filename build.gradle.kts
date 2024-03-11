b2ecc03b0cad0c2684a17b59a5206a9af4f8ece6
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-03-11 21:41:33 +0100

    world

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 }
 
 group = "com.github.angeschossen"
-version = "5.16.19"
+version = "5.16.20"
 description = "ChestProtectAPI"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
