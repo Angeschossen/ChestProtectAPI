73f3c4f52475bc68eb0b30b88a80602ff1d5abaf
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2024-03-26 09:34:50 +0100

    create protection

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 }
 
 group = "com.github.angeschossen"
-version = "5.17.1"
+version = "5.17.2"
 description = "ChestProtectAPI"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
